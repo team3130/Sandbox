9644c8b0a6cdef4d9321a53fe1c7028da6b0d0f3
Caleb Spielman <cdspielm@gmail.com>
2018-12-18 08:30:12 -0600

    Update SpielmanLeapYear.java

--- src/SpielmanLeapYear.java
+++ src/SpielmanLeapYear.java
@@ -1,20 +1,13 @@
-import org.jetbrains.annotations.Contract;
-
 import java.util.Scanner;
 
-/**
- * TThis file will check if a year is a leap year.
- * @author Eastan
- * @version 10/11/18
- */
 public class SpielmanLeapYear {
 
     @Contract(pure = true)
-    public static boolean isLeapYear(int year){
+    public static boolean isLeapYear(int year) {
         //TODO: put your code here, your method should take a integer year and return whether or not it is a leap year. True if it is, false if it isn't
 
         {
-        if (year % 4 == 0 && year % 100 != 0);
+            if (year % 4 == 0 && year % 100 != 0) ;
         }
         return true;
     }
