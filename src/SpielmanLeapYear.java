9b15b28a434df0a5ef99e74ef49ce1af9e770e6e
cdspielm03 <cdspielm@gmail.com>
2019-01-02 07:33:14 -0600

    Update SpielmanLeapYear.java

--- src/SpielmanLeapYear.java
+++ src/SpielmanLeapYear.java
@@ -6,16 +6,16 @@ public class SpielmanLeapYear {
     public static boolean isLeapYear(int year) {
         //TODO: put your code here, your method should take a integer year and return whether or not it is a leap year. True if it is, false if it isn't
 
-        if (year % 4 == 0 && year % 100 == 0);{
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 100 != 0) {
             return true;
+        } else if (year % 400 != 0) {
+            return false;
         } else {
-            if (year % 4 != 0 && year % 100 != 0) ;
-            {
-                return false;
-            }
-
+            return true;
         }
-        }
+    }
     public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Type in a year.");
