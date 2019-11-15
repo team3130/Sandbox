8e505b07a970bc3eba4d23f471965cf946ca52a6
TomasValdes <33334918+TomasValdes@users.noreply.github.com>
2019-11-15 16:48:08 -0600

    Set up for 2019

--- src/Ryan_Hello_World.java
+++
@@ -1,6 +0,0 @@
-public class Ryan_Hello_World {
-
-    public static void main(String[] args) {
-        System.out.println("Hi World!");
-    }
-}
--- src/TracyHelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class TracyHelloWorld {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-    }
-}
