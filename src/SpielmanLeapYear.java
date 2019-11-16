3fcc5826bb9b169b0001002896194b955f9d27bc
Caleb Spielman <cdspielm@gmail.com>
2019-11-15 19:59:49 -0600

    Create SpielmanLeapYear.java

---
+++ src/SpielmanLeapYear.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * TThis file will check if a year is a leap year.
+ * @author Eastan
+ * @version 10/11/18
+ */
+public class LeapYearTemplate {
+
+    public static boolean isLeapYear(int year){
+        if(isLeapYear(int year % 4) == 0)
+        { return true; }
+        return true;
+    }
+        return true;
+    }
+
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type in a year.");
+        int testYear = sc.nextInt();
+        if(LeapYearTemplate.isLeapYear(testYear) == true){
+            System.out.println("The year " + testYear + " is a leap year");
+        }else{
+            System.out.println("The year " + testYear + " is not a leap year");
+        }
+    }
+}
